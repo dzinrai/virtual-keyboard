99fe01805ada12970a2ece848e5c946343e84d69
dzinrai <dzinrai@mail.ru>
2020-04-05 05:36:46 +0300

    feat: add all symbols to keyboard

--- keyboardClass.js
+++ keyboardClass.js
@@ -7,6 +7,8 @@ class Keyboard {
         this.btnList = buttonList;
         this.domElement = container;
         this.upCase = false;
+        this.shiftPressed = false;
+        this.capsPressed = false;
         this.keyboardRows = [];
         this.langChange = [false, false];
         for (let row = 0; row <= 4; row += 1) {
@@ -17,71 +19,101 @@ class Keyboard {
         this.targetOfKeyboard = document.getElementById('textarea');
         this.domElement.addEventListener('click', (event) => {
             console.log(event.target);
-            if (event.target.tagName !== 'BUTTON') return false;
-            // console.log(`textarea.value=${textarea.value},value=${this.value}`);
+            if (event.target.tagName !== 'BUTTON' || event.target.id === 'ShiftLeft' || event.target.id === 'ShiftRight') return false;
             this.triggerKeyboardEvent(document, 'keydown', event.target.id);
             setTimeout(() => { this.triggerKeyboardEvent(document, 'keyup', event.target.id); }, 100);
         });
         document.addEventListener('keydown', (event) => {
             this.targetOfKeyboard.focus();
             this.targetOfKeyboard.selectionStart = this.posIn;
-            // console.log(event);
             if (event.code) console.log(`key=${event.key},code=${event.code}`);
             else console.log(`key=${this.btnList[event.keyCode].value},code=${event.keyCode}`);
             const keyCode = event.code ? event.code : event.keyCode;
             if (this.btnList[keyCode].inputTypeValue) {
                 event.preventDefault();
-
-                let value = this.language === 'en' ? this.btnList[keyCode].value : this.btnList[keyCode].altValue;
-                if (Array.isArray(value)) value = value[0];
+                let value;
+                if (this.btnList[keyCode].multiLang) {
+                    value = this.language === 'en' ? this.btnList[keyCode].value : this.btnList[keyCode].altValue;
+                } else value = this.btnList[keyCode].value;
+                let secondValue = null;
+                if (Array.isArray(value)) [value, secondValue] = value;
+                console.log(value, secondValue);
+                //
                 if (value === 'backspace') {
                     this.targetOfKeyboard.value = this.valueApp('backspace');
                     this.targetOfKeyboard.focus();
                 } else if (value === 'delete') {
                     this.targetOfKeyboard.value = this.valueApp('delete');
                     this.targetOfKeyboard.focus();
-                } else this.targetOfKeyboard.value = this.valueApp('none', value);
+                } else if (!this.upCase) {
+                    this.targetOfKeyboard.value = this.valueApp('none', String(value).toLowerCase());
+                } else if (this.shiftPressed && secondValue !== null) {
+                    this.targetOfKeyboard.value = this.valueApp('none', String(secondValue));
+                } else if (this.upCase) {
+                    this.targetOfKeyboard.value = this.valueApp('none', String(value).toUpperCase());
+                }
             } else if (this.btnList[keyCode].name === 'ArrowLeft') {
                 event.preventDefault();
                 this.posIn = this.posIn > 0 ? this.posIn - 1 : 0;
             } else if (this.btnList[keyCode].name === 'ArrowRight') {
                 event.preventDefault();
                 this.posIn = this.posIn < this.targetOfKeyboard.value.length ? this.posIn + 1 : this.posIn;
-            } else if (this.btnList[keyCode].name === 'ShiftLeft' || this.btnList[keyCode].name === 'ShiftRight') {
-                this.langChange[0] = true;
-                this.upCase = true;
+            } else if (this.btnList[keyCode].name === 'ArrowUp') {
+                event.preventDefault();
+            } else if (this.btnList[keyCode].name === 'ArrowDown') {
+                event.preventDefault();
+            } else if ((this.btnList[keyCode].name === 'ShiftLeft' || this.btnList[keyCode].name === 'ShiftRight') && !this.shiftPressed && !this.btnList[keyCode].locked) {
+                this.shiftPressed = true;
                 this.changeCase();
-            } else if (this.btnList[keyCode].name === 'AltLeft' || this.btnList[keyCode].name === 'AltRight') {
-                this.langChange[1] = true;
-                if (this.langChange[0]) {
+                if (this.btnList[keyCode].name === 'ShiftLeft') this.btnList.ShiftRight.locked = true;
+                else this.btnList.ShiftLeft.locked = true;
+            } else if (this.btnList[keyCode].name === 'ControlLeft' || this.btnList[keyCode].name === 'ControlRight') {
+                if (this.shiftPressed) {
                     this.changeLanguage();
                 }
-                console.log(this);
             } else if (this.btnList[keyCode].name === 'CapsLock') {
-                this.upCase = !this.upCase;
+                this.capsPressed = true;
                 this.changeCase();
             }
             this.targetOfKeyboard.selectionStart = this.posIn;
             this.targetOfKeyboard.selectionEnd = this.posIn;
             this.btnList[keyCode].addClass('active');
-            // console.log(textarea.value);
+            console.log(this.targetOfKeyboard.selectionStart);
         });
+        // shifr key mouse controls
+        this.domElement.addEventListener('mousedown', (event) => {
+            if (event.target.id === 'ShiftLeft' || event.target.id === 'ShiftRight') {
+                this.shiftPressed = true;
+                this.changeCase();
+                if (this.btnList[event.target.id].name === 'ShiftLeft') this.btnList.ShiftRight.locked = false;
+                else this.btnList.ShiftLeft.locked = false;
+            }
+        });
+        this.domElement.addEventListener('mouseup', (event) => {
+            if (event.target.id === 'ShiftLeft' || event.target.id === 'ShiftRight') {
+                this.shiftPressed = false;
+                this.changeCase();
+                if (this.btnList[event.target.id].name === 'ShiftLeft') this.btnList.ShiftRight.locked = false;
+                else this.btnList.ShiftLeft.locked = false;
+            }
+        });
+        //
         this.targetOfKeyboard.addEventListener('blur', (event) => {
             this.posIn = this.targetOfKeyboard.selectionStart;
         });
         document.addEventListener('keyup', (event) => {
             const keyCode = event.code ? event.code : event.keyCode;
             if (this.btnList[keyCode].name === 'ShiftLeft' || this.btnList[keyCode].name === 'ShiftRight') {
-                this.langChange[0] = false;
-                this.upCase = false;
+                this.shiftPressed = false;
                 this.changeCase();
-                console.log(this.upCase);
+                if (this.btnList[keyCode].name === 'ShiftLeft') this.btnList.ShiftRight.locked = false;
+                else this.btnList.ShiftLeft.locked = false;
             }
+            console.log(keyCode);
             this.btnList[keyCode].removeClass('active');
         });
         this.targetOfKeyboard.addEventListener('click', (event) => {
             this.posIn = this.targetOfKeyboard.selectionStart;
-            console.log(this.targetOfKeyboard.selectionStart);
         });
         this.totalNumberOfKeys = this.btnList.length;
         this.rowNumber = 0;
@@ -116,7 +148,6 @@ class Keyboard {
     valueApp(mod, value_) {
         let valueBefore = '';
         let value = value_ === undefined ? '' : value_;
-        console.log(mod, value, this.posIn);
         let valueAfter = '';
         if (mod === 'backspace') valueBefore = ''.concat(this.targetOfKeyboard.value).slice(0, this.targetOfKeyboard.selectionStart - 1);
         else valueBefore = ''.concat(this.targetOfKeyboard.value).slice(0, this.targetOfKeyboard.selectionStart);
@@ -132,7 +163,6 @@ class Keyboard {
         }
         this.targetOfKeyboard.selectionStart = this.posIn;
         this.targetOfKeyboard.selectionEnd = this.posIn;
-        console.log(this.posIn);
         return value;
     }
 
@@ -142,29 +172,25 @@ class Keyboard {
         this.languageAlter = langTemp;
         for (let i = 0; i < this.buttonListArray.length; i += 1) {
             if (this.buttonListArray[i].multiLang && this.language === 'ru') {
-                this.buttonListArray[i].domElement.innerHTML = this.buttonListArray[i].altValue;
-                console.log(this.buttonListArray);
+                this.buttonListArray[i].domElement.innerHTML = Array.isArray(this.buttonListArray[i].altValue) ? this.buttonListArray[i].altValue.join('') : this.buttonListArray[i].altValue;
             } else if (this.buttonListArray[i].multiLang && this.language === 'en') {
-                this.buttonListArray[i].domElement.innerHTML = this.buttonListArray[i].value;
+                this.buttonListArray[i].domElement.innerHTML = Array.isArray(this.buttonListArray[i].value) ? this.buttonListArray[i].value.join('') : this.buttonListArray[i].value;
             }
         }
     }
 
-    changeCase() {
+    changeCase(upperCase) {
+        this.upCase = upperCase === undefined ? !this.upCase : upperCase;
         if (this.upCase) {
             for (let i = 0; i < this.buttonListArray.length; i += 1) {
-                if (this.buttonListArray[i].multiLang) {
-                    this.buttonListArray[i].value = String(this.buttonListArray[i].value).toUpperCase();
-                    this.buttonListArray[i].altValue = String(this.buttonListArray[i].altValue).toUpperCase();
+                if (this.buttonListArray[i].multiLang && !this.buttonListArray[i].digitType) {
                     this.buttonListArray[i].domElement.innerHTML = String(this.buttonListArray[i].domElement.innerHTML).toUpperCase();
-                    //console.log(this.buttonListArray);
+                    // console.log(this.buttonListArray);
                 }
             }
         } else {
             for (let i = 0; i < this.buttonListArray.length; i += 1) {
-                if (this.buttonListArray[i].multiLang) {
-                    this.buttonListArray[i].value = String(this.buttonListArray[i].value).toLowerCase();
-                    this.buttonListArray[i].altValue = String(this.buttonListArray[i].altValue).toLowerCase();
+                if (this.buttonListArray[i].multiLang && !this.buttonListArray[i].digitType) {
                     this.buttonListArray[i].domElement.innerHTML = String(this.buttonListArray[i].domElement.innerHTML).toLowerCase();
                     //console.log(this.buttonListArray);
                 }
--- script.js
+++ script.js
@@ -15,20 +15,22 @@ const keyboard = new Keyboard('en', 'ru', {}, keyboardContainer);
 //
 const btns = {};
 class Button {
-    constructor(name, innerHtml, altValue, multiLang = false) {
+    constructor(name, value, altValue, multiLang = false) {
         this.name = name;
-        this.domElement = document.createElement('BUTTON');
-        this.domElement.innerHTML = innerHtml;
-        this.value = innerHtml;
+        this.value = value;
         this.altValue = altValue;
+        this.domElement = document.createElement('BUTTON');
+        if (keyboard.language === 'en') this.domElement.innerHTML = Array.isArray(value) ? value.join(' ') : value;
+        else this.domElement.innerHTML = Array.isArray(altValue) ? altValue.join('') : altValue;
         this.inputTypeValue = true;
         this.workerType = false;
         this.multiLang = multiLang;
+        this.digitType = Number.isInteger(value);
         this.domElement.classList.add('keyboard__button');
         this.domElement.classList.add('button__'.concat(name.toLowerCase()));
         this.domElement.setAttribute('id', name);
         this.addToKeyboard(keyboard);
-        if (name === 'Tab' || name === 'ControlLeft' || name === 'ControlRight' || name === 'ShiftLeft' || name === 'ShiftRight' || name === 'AltLeft' || name === 'AltRight' || name === 'ArrowUp' || name === 'ArrowLeft' || name === 'ArrowRight' || name === 'ArrowDown' || name === 'CapsLock' || name === 'OSLeft') {
+        if (name === 'ControlLeft' || name === 'ControlRight' || name === 'ShiftLeft' || name === 'ShiftRight' || name === 'AltLeft' || name === 'AltRight' || name === 'ArrowUp' || name === 'ArrowLeft' || name === 'ArrowRight' || name === 'ArrowDown' || name === 'CapsLock' || name === 'OSLeft') {
             this.inputTypeValue = false;
             this.workerType = true;
         } else if (name === 'Space') {
@@ -43,7 +45,11 @@ class Button {
         } else if (name === 'Delete') {
             this.value = 'delete';
             this.workerType = true;
+        } else if (name === 'Tab') {
+            this.value = '\t';
+            this.workerType = true;
         }
+        this.locked = false;
     }
 
     addToKeyboard(keyBoard) {
@@ -61,15 +67,16 @@ class Button {
 }
 
 // Creating buttons
-btns.Backquote = new Button('Backquote', '`', 'ё');
+btns.Backquote = new Button('Backquote', ['`', '~'], 'ё', true);
 for (let i = 1; i <= 10; i += 1) {
+    const symb = [['!', '!'], ['@', '"'], ['#', '№'], ['$', ';'], ['%', '%'], ['^', ':'], ['&', '?'], ['*', '*'], ['(', '('], [')', ')']];
     // digits
     const num = i < 10 ? i : 0;
     const btn = 'Digit'.concat(num);
-    btns[btn] = new Button(btn, num);
+    btns[btn] = new Button(btn, [num, symb[i - 1][0]], [num, symb[i - 1][1]], true);
 }
-btns.Minus = new Button('Minus', '-', '_');
-btns.Equal = new Button('Equal', '=', '+');
+btns.Minus = new Button('Minus', ['-', '_'], ['-', '_']);
+btns.Equal = new Button('Equal', ['=', '+'], ['=', '+']);
 btns.Backspace = new Button('Backspace', 'Backspace');
 btns.Tab = new Button('Tab', 'Tab');
 const keyList1 = 'qwertyuiop';
@@ -78,8 +85,8 @@ for (let i = 0; i < keyList1.length; i += 1) {
     const btn = 'Key'.concat(keyList1[i].toUpperCase());
     btns[btn] = new Button(btn, keyList1[i], keyList1alt[i], true);
 }
-btns.BracketLeft = new Button('BracketLeft', '{', 'х', true);
-btns.BracketRight = new Button('BracketRight', '}', 'ъ', true);
+btns.BracketLeft = new Button('BracketLeft', ['[', '{'], 'х', true);
+btns.BracketRight = new Button('BracketRight', [']', '}'], 'ъ', true);
 btns.Delete = new Button('Delete', 'Del');
 btns.CapsLock = new Button('CapsLock', 'Caps');
 const keyList2 = 'asdfghjkl';
@@ -88,8 +95,8 @@ for (let i = 0; i < keyList2.length; i += 1) {
     const btn = 'Key'.concat(keyList2[i].toUpperCase());
     btns[btn] = new Button(btn, keyList2[i], keyList2alt[i], true);
 }
-btns.Semicolon = new Button('Semicolon', ';', 'ж', true);
-btns.Quote = new Button('Quote', '\'', 'э', true);
+btns.Semicolon = new Button('Semicolon', [';', ':'], 'ж', true);
+btns.Quote = new Button('Quote', ['\'', '"'], 'э', true);
 btns.Backslash = new Button('Backslash', ['\\', '|'], ['\\', '/'], true);
 btns.Enter = new Button('Enter', 'Enter');
 btns.ShiftLeft = new Button('ShiftLeft', 'Shift');
@@ -100,9 +107,9 @@ for (let i = 0; i < keyList3.length; i += 1) {
     const btn = 'Key'.concat(keyList3[i].toUpperCase());
     btns[btn] = new Button(btn, keyList3[i], keyList3alt[i], true);
 }
-btns.Comma = new Button('Comma', ',', 'б', true);
-btns.Period = new Button('Period', '.', 'ю', true);
-btns.Slash = new Button('Slash', '/');
+btns.Comma = new Button('Comma', [',', '<'], 'б', true);
+btns.Period = new Button('Period', ['.', '>'], 'ю', true);
+btns.Slash = new Button('Slash', ['/', '?'], ['.', ','], true);
 btns.ArrowUp = new Button('ArrowUp', '^');
 btns.ShiftRight = new Button('ShiftRight', 'Shift');
 btns.ControlLeft = new Button('ControlLeft', 'Ctrl');
@@ -120,5 +127,5 @@ keyboard.normalizeIt();
 console.log(keyboard.btnList);
 const text = document.createElement('DIV');
 text.classList.add('info');
-text.innerHTML = 'Shift + Alt';
+text.innerHTML = 'Shift + Ctrl';
 body.appendChild(text);
