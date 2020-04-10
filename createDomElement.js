3cb273245930ccdf88ec6095be1511ca15b12c9a
dzinrai <dzinrai@mail.ru>
2020-04-10 17:36:12 +0300

    fix: bug with 'null' classes

--- createDomElement.js
+++ createDomElement.js
@@ -3,8 +3,9 @@ function createDomElement(tagName, id, className, targetForAppend, innerHTML) {
     // id
     if (id) element.setAttribute('id', id);
     // classes
-    if (!Array.isArray(className)) element.classList.add(className);
-    else className.forEach((classTag) => { element.classList.add(classTag); });
+    if (Array.isArray(className)) {
+        className.forEach((classTag) => { element.classList.add(classTag); });
+    } else if (className) element.classList.add(className);
     // append to
     if (targetForAppend) targetForAppend.appendChild(element);
     // innerHTML
